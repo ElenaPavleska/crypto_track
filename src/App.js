4eea0a584e06a22606f5aa0adf81929b710105f2
Elena <eli92pavleska@gmail.com>
2019-07-01 21:52:34 +0200

    NotFound Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import  NavBar  from "./components/navBar";
-import  Cryptocurrencies  from "./components/cryptocurrencies";
-import  CryptocurrencyDetails  from "./components/cryptocurrencyDetails";
+import NavBar  from "./components/navBar";
+import Cryptocurrencies  from "./components/cryptocurrencies";
+import CryptocurrencyDetails  from "./components/cryptocurrencyDetails";
+import NotFound from "./components/notFound";
 import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
@@ -17,7 +18,9 @@ class App extends Component {
                     <Switch>
                         <Route path="/cryptocurrencies/:id" component={CryptocurrencyDetails} />
                         <Route path="/cryptocurrencies" component={Cryptocurrencies} />
+                        <Route path="/not-found" component={NotFound} />
                         <Redirect from="/" exact to="/cryptocurrencies" />
+                        <Redirect to="/not-found" />
                     </Switch>
                 </main>
             </React.Fragment>
